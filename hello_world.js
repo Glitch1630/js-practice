77c5fa8a2ec90d542d5d148dd35a8ef4f17d3a89
Abhirami <sreesalini9495@gmail.com>
2026-07-20 17:42:35 +0530

    Updated hello_world with name variable

--- hello_world.js
+++ hello_world.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+
+let name="Alice" ;
+if (name ==="Alice"){
+    console.log("Hello Alice");
+}
+else if(name===null || name ===undefined) {
+    console.log("Hello World");
+
+}
